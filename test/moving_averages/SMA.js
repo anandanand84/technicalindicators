aa852f44c6cc984b97a005b83c9553f6b3d6485c
Anand Aravindan <anand.anand84@gmail.com>
2016-12-24 15:39:00 -0600

    Add test for Issue 1 and new formatting input

--- test/moving_averages/SMA.js
+++ test/moving_averages/SMA.js
@@ -42,4 +42,14 @@ describe('SMA (Simple Moving Average)', function() {
     });
     assert.deepEqual(results, expectResult, 'Wrong Results while getting results');
   })
+
+  it('should be able to get SMA for low values(issue 1)', function() {
+    let expectedResult = [ 0.002, 0.00275, 0.0025, 0.003, 0.003, 0.0025 ];
+    assert.deepEqual(SMA.calculate({period : 4, values : [0.001, 0.003, 0.001, 0.003, 0.004, 0.002, 0.003, 0.003, 0.002]}), expectedResult, 'Wrong Results');
+  })
+  
+  it('Passing format function should format the results appropriately', function() {
+    let expectedResult = [ 0.002, 0.003, 0.003, 0.003, 0.003, 0.003 ];
+    assert.deepEqual(SMA.calculate({period : 4, values : [0.001, 0.003, 0.001, 0.003, 0.004, 0.002, 0.003, 0.003, 0.002], format : (val) => { return val.toPrecision(1) }}), expectedResult, 'Wrong Results');
+  })
 })
