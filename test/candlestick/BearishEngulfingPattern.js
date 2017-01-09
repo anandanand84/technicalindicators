d41de475e7d56d4c69ce35f0ae06ae2b913f6ae0
Anand Aravindan <anand.anand84@gmail.com>
2017-01-08 20:09:29 -0600

    Fix strategy not defined issue

--- test/candlestick/BearishEngulfingPattern.js
+++ test/candlestick/BearishEngulfingPattern.js
@@ -9,6 +9,13 @@ var twoDayBearishInput = {
   low: [20.82,14.93],
 }
 
+var oneDayBearishInput = {
+  open: [21.44],
+  high: [25.10],
+  close: [23.25],
+  low: [20.82],
+}
+
 describe('BearishEngulfingPattern : ', function() {
   before(function() {
     var imageBuffer = drawCandleStick(twoDayBearishInput);
@@ -18,7 +25,12 @@ describe('BearishEngulfingPattern : ', function() {
    var bearishEngulfingPattern = new BearishEngulfingPattern ();
    var result        = bearishEngulfingPattern.hasPattern(twoDayBearishInput);
    assert.deepEqual(result, true, 'Invalid result for BearishEngulfingPattern');
-   
+  });
+  
+  it('Should return false if less data is provided', function() {
+   var bearishEngulfingPattern = new BearishEngulfingPattern ();
+   var result        = bearishEngulfingPattern.hasPattern(oneDayBearishInput);
+   assert.deepEqual(result, false, 'Invalid result for BearishEngulfingPattern');
   });
 })
 
