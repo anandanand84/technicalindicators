aa119ef966b2e5ef6039bdd9303a9ec0175c140e
Marcus Bernal <mabernal2526@gmail.com>
2018-08-09 21:19:12 -0600

    Added tests for Doji precision.
    Includes changes to test for:
    - Doji
    - DragonFlyDoji
    - GraveStoneDoji

--- test/candlestick/GraveStoneDoji.js
+++ test/candlestick/GraveStoneDoji.js
@@ -11,6 +11,14 @@ var input = {
   
 }
 
+var inputDot = {
+  open: [30.10],
+  high: [30.11],
+  close: [30.10],
+  low: [30.09],
+
+}
+
 describe('GraveStoneDoji : ', function() {
    before(function() {
     var imageBuffer = drawCandleStick(input);
@@ -20,5 +28,10 @@ describe('GraveStoneDoji : ', function() {
    var graveStoneDoji = new GraveStoneDoji();
    var result = graveStoneDoji.hasPattern(input);
    assert.deepEqual(result, true, 'Invalid result for GraveStoneDoji');
+  });
+  it('Check whether the supplied data has GraveStoneDoji pattern', function() {
+   var graveStoneDoji = new GraveStoneDoji();
+   var result = graveStoneDoji.hasPattern(inputDot);
+   assert.deepEqual(result, false, 'Invalid result for a single point Doji');
   });
 })
