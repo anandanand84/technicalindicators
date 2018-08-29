44ad088d98b817927ad146f99542a285626f2abf
Marcus Bernal <mabernal2526@gmail.com>
2018-08-29 17:18:37 -0600

    Added pattern detection for hammers.
    Inludes:
    - HammerPattern:
      - Returns true if all of the following do:
        - First three days of closing data (indexes 0, 1, and 2) experience more losses than gains
        - Fourth day data is of any hammer candlestick form: Bearish, Bearish Inverted, Bullish, Bullish Inverted
        - Fifth day data is bullish and closes higher than the fourth day's close
    - HammerPatternUnconfirmed:
      - Returns true if all of the following do:
        - First four days of closing data (indexes 0, 1, 2, and 3) experience more losses than gains
        - Fifth day data is of any hammer candlestick form: Bearish, Bearish Inverted, Bullish, Bullish Inverted
      - This class extends the HammerPattern class

---
+++ src/candlestick/HammerPatternUnconfirmed.ts
@@ -0,0 +1,20 @@
+import StockData from '../StockData';
+import HammerPattern from './HammerPattern';
+
+export default class HammerPatternUnconfirmed extends HammerPattern {
+    constructor() {
+        super();
+        this.name = 'HammerPatternUnconfirmed';
+        this.requiredCount = 5;
+    }
+
+    logic (data:StockData) {
+        let isPattern = this.downwardTrend(data, false);
+        isPattern = isPattern && this.includesHammer(data, false);
+        return isPattern;
+   }
+}
+
+export function hammerpatternunconfirmed(data:StockData) {
+  return new HammerPatternUnconfirmed().hasPattern(data);
+}
