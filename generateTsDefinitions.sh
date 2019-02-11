562131d1bf372d2e3dd9d47b3c78f787f9d4b519
Roberto Previdi <hariseldon78@gmail.com>
2019-02-11 02:47:00 +0100

    fix duplicate declarations in generated.d.ts (#165)
    * add dts-bundle into installation
    
    * remove duplicated declaration
    
    * fixed declarations generation script

--- generateTsDefinitions.sh
+++ generateTsDefinitions.sh
@@ -1,2 +1,4 @@
-dts-bundle --name indicators --main declarations/index.d.ts --out generated.d.ts --baseDir declarations --outputAsModuleFolder
-sed -i '' 's/default class/class/g' declarations/generated.d.ts
+#!/usr/bin/env bash
+./node_modules/.bin/dts-bundle --name indicators --main declarations/index.d.ts --out generated.d.ts --baseDir declarations --outputAsModuleFolder
+sed -i 's/default class/class/g' declarations/generated.d.ts
+sed -i 's/export { .* };//g' declarations/generated.d.ts
