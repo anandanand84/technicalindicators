4e2c2c1ebfdc86d48f6d4281d6a9f6f6f2911c7c
CryptenseDev <36669352+CryptenseDev@users.noreply.github.com>
2019-11-15 15:46:22 +0100

    BullishSpinningTop fix
    Tests of the direction of the candle for pattern validation

--- src/candlestick/BullishSpinningTop.ts
+++ src/candlestick/BullishSpinningTop.ts
@@ -12,12 +12,13 @@ export default class BullishSpinningTop extends CandlestickFinder {
         let daysClose = data.close[0];
         let daysHigh  = data.high[0];
         let daysLow   = data.low[0];
-
+        let isBullishSpinningTop = daysClose > daysOpen;
         let bodyLength           = Math.abs(daysClose-daysOpen);
         let upperShadowLength    = Math.abs(daysHigh-daysClose);
         let lowerShadowLength    = Math.abs(daysOpen-daysLow);
-        let isBullishSpinningTop = bodyLength < upperShadowLength && 
-                                 bodyLength < lowerShadowLength;
+        isBullishSpinningTop = isBullishSpinningTop &&
+                               bodyLength < upperShadowLength && 
+                               bodyLength < lowerShadowLength;
 
         return isBullishSpinningTop;
     }
